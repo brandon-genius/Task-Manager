659aded95234faea2ffe7dc954f5ea3353b3c87f
Brandon Britt <brandon.wpi@gmail.com>
2022-04-05 16:40:09 -1000

    class 3
    class 3

--- app.js
+++ app.js
@@ -28,11 +28,43 @@ function toggleVisibility(){
 
 }
 
+function saveTask(){
+    console.log("Register Tasks");
+
+    var imp = $("#iImportant").val();
+    var title = $("#txtTitle").val();
+    var desc = $("#txtDescription").val();
+    var color = $("#selColor").val();
+    var due = $("#dueDate").val();
+    var start = $("#startDate").val();
+    var cat = $("#selCat").val();
+
+    console.log(imp,title,desc,color,due,start,cat);
+}
+
 function init() {
   console.log("Task Manager");
 
   $("#iImportant").click(toggleImportance);
   $("#btnTogglePanel").click(toggleVisibility);
+  $("#btnSave").click(saveTask);
+}
+
+function test() {
+    $.ajax({
+        url: "https://restclass.azurewebsites.net/api/test",
+        type: "GET",
+        success: function(response){
+            console.log("server says:", response);
+        },
+        error: function(errorDetails) {
+            console.log(errorDetails);
+        }
+    });
+}
+
+function registerTask(){
+    console.log("Register Tasks");
 }
 
 window.onload = init;
